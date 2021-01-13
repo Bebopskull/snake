e21e6ce51f6dec4ae0cd899dca0b821ecd1da926
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-01-13 16:01:11 -0500

    added some text with keys q,e & x

--- input.js
+++ input.js
@@ -12,27 +12,53 @@ const handleUserInput = function(data){
 /////mouvement/////
     if (key === `w`) {
       console.log(`up`)
-      connection.write(`Move: up`); 
+      connection.write(`Move: up`);
       // setTimeout(() => {
+      //   connection.write(`Say: arriba!`);
+
       // }, 50);
     }
     if (key === `s`) {
       console.log(`down`)
-      connection.write(`Move: down`); 
+      connection.write(`Move: down`);
       // setTimeout(() => {
+      //   connection.write(`Say: Let's Go!`);  
       // }, 50);
     }
     if (key === `a`) {
       console.log(`left`);
-      connection.write(`Move: left`); 
+      connection.write(`Move: left`);
       // setTimeout(() => {
+      //   connection.write(`Say: Hey!`);  
       // }, 50);
     }
     if (key === `d`) {
       console.log(`right`);
       connection.write(`Move: right`); 
+      
+      // setTimeout(() => {
+      //   connection.write(`Say: Ho!!`); 
+      // }, 50); 
     }
-    
+    if (key === `q`) {
+     
+      connection.write(`Say: Hey!`);
+      // setTimeout(() => {
+      // }, 50);
+    }
+    if (key === `e`) {
+     
+      connection.write(`Say: Ho!`);
+      // setTimeout(() => {
+      // }, 50);
+    }
+
+    if (key === `x`) {
+     
+      connection.write(`Say: Let's Go!!`);
+      // setTimeout(() => {
+      // }, 50);
+    }
 
 
   });
