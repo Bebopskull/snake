d5c2cac09c8113e8d95ebfd7923c279760f58fe4
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-01-13 13:54:16 -0500

    separating functions, stablishing message and name at connection

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require('net');
+
+
+///connecting to the server
+const connect = function(){
+  const conn = net.createConnection({
+    host:`135.23.222.131`,
+    port:50542
+  });
+  conn.setEncoding(`utf8`);
+
+
+
+  conn.on(`data`, (data) => {
+    console.log(`Server Says: ${data}`);
+  });
+  
+  ///connect event
+  conn.on(`connect`, () => {
+    console.log(`Huzza!!! connection stablished!!!`);
+    conn.write(`Name: Ed`);
+  });
+
+
+  
+  return conn;
+};
+
+
+module.exports = { connect };
+
+
+
--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const net = require('net');
+const { connect } = require('./client.js');
 
-
-///connecting to the server
-const connect = function(){
-  const conn = net.createConnection({
-    host:`135.23.222.131`,
-    port:50542
-  });
-  conn.setEncoding(`utf8`);
-
-  conn.on(`data`, (data) => {
-    console.log(`Server Says: ${data}`);
-  });
-  
-  conn.on(`connect`, () => {
-    conn.write(`Huzza!!!`);
-  });
-  return conn;
-};
+////play sequence
+////iniciating sequence/////
 console.log(`connecting....`);
 connect();
-
-
 
